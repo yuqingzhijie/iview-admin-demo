422827e7f582df412f48540528d57eb6fba51b61
yuqing <2711676102@qq.com>
2019-01-24 17:20:07 +0800

    add login page

---
+++ src/store/module/user.js
@@ -0,0 +1,22 @@
+import Cookies from 'js-cookie'
+import cookieExpires from '_conf/app/basic'
+
+const TOKEN_KEY = 'token'
+
+export default {
+  state: {
+    userName: '',
+    userId: '',
+    avatorImgPath: ''
+  },
+  mutations: {
+    handleLogin (state, {userName, userId, token, avator}) {
+      if (token) {
+        Cookies.set(TOKEN_KEY, token, {expires: cookieExpires})
+      }
+      state.userName = userName
+      state.userId = userId
+      state.avator = avator
+    }
+  }
+}
